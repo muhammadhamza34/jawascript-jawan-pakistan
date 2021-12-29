8841b6093c99bda3ca6a1cec2719b81d31c09fb7
Muhammad Hamza <muhammadhamza514@gmail.com>
2021-12-29 20:00:18 +0500

    work complete

---
+++ public/app.js
@@ -0,0 +1,11 @@
+alert("Hello world");
+document.write("HAMZA");
+console.log("Muhammad hamza");
+var a='PAF KIET';
+var b=123;
+
+document.write(a);
+document.write(b);
+
+var cal=2+2;
+document.write(cal);
